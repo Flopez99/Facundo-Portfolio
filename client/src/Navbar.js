db7c7006306f04acf83f275973ccadb176d395c0
Facundo Lopez <facundo.lopezcamino@stonybrook.edu>
2024-07-11 14:54:34 -0400

    Added hamburger menu for mobile devices

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,41 +1,34 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './styling/Navbar.css';
 
-function Navbar(){
-    const [isOpen, setIsOpen] = useState(false);
-    const  toggleNavbar = ()=>{
-        setIsOpen(!isOpen);
-    }
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return(
-        <nav className="navBar">
-            <div className="navbar-container">
-                <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/projects">Projects</Link>
-                        </li>
-                        <li>
-                            <Link to="/playground">Playground</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="navbar-toggle" onClick={toggleNavbar}>
-                    <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'}`}></i>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navBar">
+      <div className="navbar-container">
+       
+        <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/playground">Playground</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </div>
+      <button className="navbar-toggle" onClick={toggleNavbar}>
+        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+      </button>
+    </nav>
+  );
 }
 
 export default Navbar;
