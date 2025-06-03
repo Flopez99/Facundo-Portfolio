d793930ea2f6578291f14ac05cf7f1d2d602b2a8
Facundo Lopez <facundo.lopezcamino@stonybrook.edu>
2025-06-03 14:06:17 -0400

    Added tags to projects

--- client/src/pages/page_components/ProjectCard.js
+++ client/src/pages/page_components/ProjectCard.js
@@ -1,6 +1,9 @@
 import '../../styling/ProjectPage.css';
 
+
 function ProjectCard({ project }) {
+    let technologies = project.techs.split(', ')
+
     return (
         <div className="project-card">
             <div className="img-container">
@@ -8,7 +11,17 @@ function ProjectCard({ project }) {
             </div>
             <h3 className="project-name">{project.name}</h3>
             <p className="project-description">{project.description}</p>
-            <a href={project.link} className="project-button">View</a>
+            <div className='card-footer'>
+                <div className='tech-tags'>
+                    {technologies.map((tech, index) => (
+                        <span key={index} className="tech-tag">{tech}</span>
+                    ))}
+                </div>
+                <a href={project.link} className="project-button">View</a>
+            </div>
+            
+            
+            
         </div>
     ); 
 }
